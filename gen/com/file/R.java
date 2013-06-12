cbd51a2d50d0d41caec880d25807b341f46d0a02
Bigstone <18724605739@126.com>
2013-06-12 10:48:55 +0800

    将图标更改了，添加发送功能失败
    在网上搜了一下发送的实现，发现都是一个代码，而且少了几个txt文件，不能测试
    其中TestContextProvider为其中的一个类，现在没用到，我曾试过在主文件中添加相应的代码调用这个类，没有成功，我就又删了。
    这是网址：http://blog.csdn.net/h3c4lenovo/article/details/7860276

--- gen/com/file/R.java
+++ gen/com/file/R.java
@@ -13,8 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int folder=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int text=0x7f020002;
-        public static final int zip=0x7f020003;
+        public static final int icon1=0x7f020002;
+        public static final int text=0x7f020003;
+        public static final int zip=0x7f020004;
     }
     public static final class id {
         public static final int about=0x7f06000a;
