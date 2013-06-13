fd3a77077a46bf78d10e1e22e6bd062538f95119
Bigstone <18724605739@126.com>
2013-06-13 19:01:33 +0800

    修改了功能键的菜单，加入分享功能
    将功能键菜单改成了两行三列，其中新增了粘贴、排序和分享的接口。
    没有实现发送文件的功能，参照网上的代码写了调用微博的功能。

--- gen/com/file/R.java
+++ gen/com/file/R.java
@@ -18,18 +18,21 @@ public final class R {
         public static final int zip=0x7f020004;
     }
     public static final class id {
-        public static final int about=0x7f06000a;
+        public static final int about=0x7f06000d;
         public static final int btnC=0x7f060003;
         public static final int btnE=0x7f060004;
         public static final int currentDir=0x7f060006;
-        public static final int exit=0x7f06000b;
+        public static final int exit=0x7f06000e;
         public static final int icon=0x7f060000;
         public static final int linearLayout1=0x7f060002;
         public static final int linearLayout2=0x7f060005;
         public static final int listView=0x7f060007;
         public static final int name=0x7f060001;
-        public static final int newFile=0x7f060009;
+        public static final int newFile=0x7f06000c;
+        public static final int paste=0x7f060009;
         public static final int rename=0x7f060008;
+        public static final int share=0x7f06000b;
+        public static final int sort=0x7f06000a;
     }
     public static final class layout {
         public static final int item=0x7f030000;
@@ -47,5 +50,8 @@ public final class R {
         public static final int exit=0x7f040006;
         public static final int hello=0x7f040000;
         public static final int newFile=0x7f040004;
+        public static final int paste=0x7f040007;
+        public static final int share=0x7f040009;
+        public static final int sort=0x7f040008;
     }
 }
--- src/com/explorer/MainActivity.java
+++ src/com/explorer/MainActivity.java
@@ -21,8 +21,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -46,6 +50,7 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.text.style.SuperscriptSpan;
 import android.util.Log;
 
 import com.file.R;
@@ -212,6 +217,15 @@ public class MainActivity extends Activity implements OnClickListener,
 			{
 				Log.v(Name, "level2 item.getItemId() == R.id.exit");
 				MainActivity.this.finish();
+			}else if (item.getItemId() == R.id.paste)
+			{
+				paste();
+			}else if (item.getItemId() == R.id.sort)
+			{
+				
+			}else if (item.getItemId() == R.id.share)
+			{
+				share();
 			}
 			return true;
 		}
@@ -419,7 +433,7 @@ public class MainActivity extends Activity implements OnClickListener,
 			AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
 					.setTitle("操作")
 					.setItems(new String[]
-					{ "复制", "剪切", "粘贴", "发送", "重命名", "删除", "属性" },
+					{ "复制", "剪切", "粘贴", "分享", "重命名", "删除", "属性" },
 							new DialogInterface.OnClickListener()
 							{
 								@Override
@@ -443,67 +457,11 @@ public class MainActivity extends Activity implements OnClickListener,
 										break;
 									case 2: // 粘贴
 										Log.v(Name, "level2 paste");
-										final String startPath = copyPath
-												.getAbsolutePath();
-										final String desPath = currentDir
-												.getText().toString()
-												+ "/"
-												+ copyPath.getName();
-										File[] files = new File(currentDir
-												.getText().toString())
-												.listFiles();
-										for (File file : files)
-										{
-											if (copyPath.getName().equals(
-													file.getName()))
-											{
-												Toast.makeText(
-														MainActivity.this,
-														"此文件/文件夹已存在",
-														Toast.LENGTH_SHORT)
-														.show();
-												return;
-											}
-										}
-
-										int length = (int) (getLength(copyPath) / (1024));
-										progressDialog = new ProgressDialog(
-												MainActivity.this);
-										progressDialog
-												.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-										progressDialog.setMessage("正在复制");
-										progressDialog.setMax(length);
-										progressDialog.setProgress(0);
-										progressDialog.setCancelable(false);
-										progressDialog.show();
-
-										new Thread()
-										{
-											public void run()
-											{
-												copy(startPath, desPath);
-												// 通过Message对象向原线程传递信息
-												Message message = Message
-														.obtain();
-												messageHandler
-														.sendMessage(message);
-												progressDialog.dismiss();
-												// 如果为剪切则删除对应文件/文件夹
-												if ("cut".equals(flag))
-												{
-													if (copyPath.isFile())
-													{
-														copyPath.delete();
-													} else
-													{
-														delete(copyPath);
-													}
-												}
-											}
-										}.start();
+										paste();
 										break;
-									case 3: // 发送
+									case 3: // 分享
 										Log.v(Name, "level2 send");
+										share();
 										break;
 									case 4: // 重命名
 										Log.v(Name, "level2 newName");
@@ -550,8 +508,10 @@ public class MainActivity extends Activity implements OnClickListener,
 																}
 																file.renameTo(new File(
 																		newFile));
-																loadFiles(new File(currentDir.getText()
-																		.toString()));
+																loadFiles(new File(
+																		currentDir
+																				.getText()
+																				.toString()));
 															}
 														}).create();
 										d.show();
@@ -623,6 +583,7 @@ public class MainActivity extends Activity implements OnClickListener,
 									// Toast.makeText(MainActivity.this, "" +
 									// which, Toast.LENGTH_LONG).show();
 								}
+
 							}).create();
 			dialog.show();
 			/*
@@ -631,6 +592,80 @@ public class MainActivity extends Activity implements OnClickListener,
 			 * this.finish();
 			 */
 			return true;
+		}
+
+		public void paste()
+		{
+			final String startPath = copyPath
+					.getAbsolutePath();
+			final String desPath = currentDir
+					.getText().toString()
+					+ "/"
+					+ copyPath.getName();
+			File[] files = new File(currentDir
+					.getText().toString())
+					.listFiles();
+			for (File file : files)
+			{
+				if (copyPath.getName().equals(
+						file.getName()))
+				{
+					Toast.makeText(
+							MainActivity.this,
+							"此文件/文件夹已存在",
+							Toast.LENGTH_SHORT)
+							.show();
+					return;
+				}
+			}
+
+			int length = (int) (getLength(copyPath) / (1024));
+			progressDialog = new ProgressDialog(
+					MainActivity.this);
+			progressDialog
+					.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			progressDialog.setMessage("正在复制");
+			progressDialog.setMax(length);
+			progressDialog.setProgress(0);
+			progressDialog.setCancelable(false);
+			progressDialog.show();
+
+			new Thread()
+			{
+				public void run()
+				{
+					copy(startPath, desPath);
+					// 通过Message对象向原线程传递信息
+					Message message = Message
+							.obtain();
+					messageHandler
+							.sendMessage(message);
+					progressDialog.dismiss();
+					// 如果为剪切则删除对应文件/文件夹
+					if ("cut".equals(flag))
+					{
+						if (copyPath.isFile())
+						{
+							copyPath.delete();
+						} else
+						{
+							delete(copyPath);
+						}
+					}
+				}
+			}.start();
+		}
+		
+		public void share()
+		{
+			try
+			{
+				ShareSample sample = new ShareSample();
+				sample.getShareList("分享标题", "要分享的内容哦。呵呵", this);
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
 
 		/**
@@ -919,5 +954,136 @@ public class MainActivity extends Activity implements OnClickListener,
 			return currentDesFilePath;
 
 		}
+
+		class ShareSample
+			{
+
+				/**
+				 * 获得分享列表（简单实现，只是把核心的东西写了写。不是太实用）
+				 * **/
+				public void getShareList(final String title,
+						final String content, final Activity mActivity)
+				{
+					final Map<String, ResolveInfo> appInfo = new HashMap<String, ResolveInfo>();
+					List<ResolveInfo> appList = getShareTargets(mActivity);
+					final String[] items;
+					if (appList.size() > 0)
+					{
+						for (int i = 0; i < appList.size(); i++)
+						{
+							ResolveInfo tmp_ri = (ResolveInfo) appList.get(i);
+							// ActivityInfo tmp_ai=tmp_ri.activityInfo;
+							ApplicationInfo apinfo = tmp_ri.activityInfo.applicationInfo;
+							String tmp_appName = apinfo.loadLabel(
+									mActivity.getPackageManager()).toString();
+							// 这里就做了一个简单的得到应用名称判断， 实际情况根据 ResolveInfo 这东东
+							// 可以得到很多很多有用的东东哦。
+							if (!tmp_appName.equals("微博"))
+							{
+								continue;
+							}
+							appInfo.put(tmp_appName, tmp_ri);
+						}
+						items = new String[appInfo.size()];
+						int j = 0;
+						for (Map.Entry<String, ResolveInfo> myEntry : appInfo
+								.entrySet())
+						{
+							items[j] = myEntry.getKey();
+							j++;
+						}
+
+					} else
+					{
+						Toast.makeText(mActivity.getApplicationContext(),
+								"暂无分享应用", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					if (appInfo.size() < 1)
+					{
+						Toast.makeText(mActivity.getApplicationContext(),
+								"暂无分享应用", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					/**
+					 * 这里用对话框简单的一个实现，实际开发中应该要单独抽取出来封装成一个自定义的列表。
+					 * 
+					 * **/
+					new AlertDialog.Builder(mActivity)
+							.setTitle("选择分享")
+							.setSingleChoiceItems(items, 0,
+									new DialogInterface.OnClickListener()
+									{
+										public void onClick(
+												DialogInterface dialog, int item)
+										{
+
+											CreateShare(title, content,
+													mActivity,
+													appInfo.get(items[item]));
+											// CreateShare(title,content,mActivity);
+											dialog.cancel();
+										}
+
+									}).show();// 显示对话框
+
+				}
+
+				/**
+				 * 实现自定义分享功能(主要就是启动对应的App)
+				 * **/
+				private void CreateShare(String title, String content,
+						Activity activity, ResolveInfo appInfo)
+				{
+
+					try
+					{
+						Intent shareIntent = new Intent(Intent.ACTION_SEND);
+						shareIntent.setComponent(new ComponentName(
+								appInfo.activityInfo.packageName,
+								appInfo.activityInfo.name));
+						// 这里就是组织内容了，
+						// shareIntent.setType("text/plain");
+						shareIntent.setType("image/*");
+						shareIntent.putExtra(Intent.EXTRA_TEXT, content);
+						shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						activity.startActivity(shareIntent);
+
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+						// TODO: handle exception
+					}
+
+				}
+
+				/**
+				 * 默认分享列表（网上一大堆）
+				 * **/
+				private void CreateShare(String title, String content,
+						Activity activity)
+				{
+					Intent intent = new Intent(Intent.ACTION_SEND);
+					intent.setType("image/*");
+					intent.putExtra(Intent.EXTRA_SUBJECT, title);
+					intent.putExtra(Intent.EXTRA_TEXT, content);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					activity.startActivity(Intent.createChooser(intent, title));
+
+				}
+
+				/**
+				 * 获得所有带Intent.ACTION_SEND的应用列表。 ResolveInfo 这个东东真不错。
+				 * **/
+				private List<ResolveInfo> getShareTargets(Activity activity)
+				{
+					Intent intent = new Intent(Intent.ACTION_SEND, null);
+					intent.addCategory(Intent.CATEGORY_DEFAULT);
+					intent.setType("text/plain");
+					PackageManager pm = activity.getPackageManager();
+					return pm.queryIntentActivities(intent,
+							PackageManager.COMPONENT_ENABLED_STATE_DEFAULT);
+				}
+			}
 
 	}
